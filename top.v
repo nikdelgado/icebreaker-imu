5933fba6360cdc1f4fd800bffdc537f33c97b005
nikdelgado <nikdelgado@icloud.com>
2026-06-28 16:09:38 -0600

    read 16-bit accel data, print hex via uart

--- top.v
+++ top.v
@@ -11,19 +11,19 @@ module top (
 localparam IDLE = 2'd0, ASSERT = 2'd1, TRANSFER = 2'd2, DONE = 2'd3;
 
 reg [7:0]   uart_tx_rate_counter = 0;
-reg [1:0]   uart_char_index = 0;
+reg [2:0]   uart_char_index = 0;
 reg [9:0]   uart_shift_register = 10'h3FF;
 reg [5:0]   uart_bit_counter = 0;
-reg [15:0]  spi_shift_register;
+reg [23:0]  spi_shift_register;
 reg [4:0]   spi_bit_counter = 0;
-reg [7:0]   spi_result = 0;
+reg [15:0]   spi_result = 0;
 reg         spi_rx_bit = 0;
 reg         spi_woken = 0;
 reg [2:0]   clock_divider = 0;
 reg [1:0]   state = IDLE;
 
 assign TX = uart_shift_register[0];
-assign spi_mosi = spi_shift_register[15];
+assign spi_mosi = spi_shift_register[23];
 
 function [7:0] hex_to_char(input [3:0] nibble);
     if (nibble < 10) 
@@ -32,11 +32,13 @@ function [7:0] hex_to_char(input [3:0] nibble);
         hex_to_char = "A" + (nibble - 10);
 endfunction
 
-function [7:0] get_tx_byte (input [1:0] idx);
+function [7:0] get_tx_byte (input [2:0] idx);
     case (idx)
-        2'd0: get_tx_byte = hex_to_char(spi_result[7:4]);
-        2'd1: get_tx_byte = hex_to_char(spi_result[3:0]);
-        2'd2: get_tx_byte = 8'h0D;
+        3'd0: get_tx_byte = hex_to_char(spi_result[15:12]);
+        3'd1: get_tx_byte = hex_to_char(spi_result[11:8]);
+        3'd2: get_tx_byte = hex_to_char(spi_result[7:4]);
+        3'd3: get_tx_byte = hex_to_char(spi_result[3:0]);
+        3'd4: get_tx_byte = 8'h0D;
         default: get_tx_byte = 8'h0A;
     endcase
 endfunction
@@ -51,7 +53,11 @@ always @(posedge CLK) begin
         if (uart_bit_counter == 10) begin
             uart_bit_counter <= 0;
             uart_shift_register <= {1'b1, get_tx_byte(uart_char_index), 1'b0};
-            uart_char_index <= uart_char_index + 1;
+            if (uart_char_index == 5) begin
+                uart_char_index <= 0;
+            end else begin
+                uart_char_index <= uart_char_index + 1;
+            end
 
         end else begin
             uart_bit_counter <= uart_bit_counter + 1;
@@ -65,9 +71,9 @@ always @(posedge CLK) begin
         case (state)
             IDLE: begin
                 if (!spi_woken) begin
-                    spi_shift_register <= 16'h0601; // write PWR_MGMT_1 = 0x01
+                    spi_shift_register <= 24'h060100; // write PWR_MGMT_1 = 0x01
                 end else begin
-                    spi_shift_register <= 16'hAD00; // read 
+                    spi_shift_register <= 24'hAD0000; // read 
                 end
 
                 spi_cs <= 1;
@@ -85,17 +91,17 @@ always @(posedge CLK) begin
                 if (spi_sclk == 0) begin
                     spi_rx_bit <= spi_miso;
                 end else begin
-                    spi_shift_register <= {spi_shift_register[14:0], spi_rx_bit};
+                    spi_shift_register <= {spi_shift_register[22:0], spi_rx_bit};
                     spi_bit_counter <= spi_bit_counter + 1;
 
-                    if (spi_bit_counter == 15) begin
+                    if (spi_bit_counter == 23) begin
                         state <= DONE;
                     end
                 end
 
             end DONE: begin
                 spi_cs <= 1;
-                spi_result <= spi_shift_register[7:0];
+                spi_result <= spi_shift_register[15:0];
                 spi_woken <= 1;
                 state <= IDLE;
             end
